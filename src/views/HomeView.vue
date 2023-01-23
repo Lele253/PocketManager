2230a41399a20e222beb7ff453cb0c53299873d8
Lele253 <120642684+Lele253@users.noreply.github.com>
2023-01-23 10:02:29 +0100

    update

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,38 @@
 <template>
   <!-- Farbpalette -->
   <div v-if="user">
-    <Icon class="farbIcon" @click="$store.state.colorAktive = !$store.state.colorAktive"  icon="cil:color-palette" />
-    <ColorComponent style="position: absolute; z-index: 300;left: 13%; top: 100px;" v-if="$store.state.colorAktive"></ColorComponent>
+
+
+    <v-dialog
+        v-model="dialog"
+        persistent
+        max-width="290"
+    >
+      <template v-slot:activator="{ on, attrs }">
+        <Icon class="farbIcon"
+              @click="dialog = true" icon="cil:color-palette" v-bind="attrs"
+              v-on="on"/>
+      </template>
+      <v-card class="dialogCard">
+        <v-card-title class="text-h5">
+          Wähle deine eigenen Farbe aus
+        </v-card-title>
+        <v-card-text>
+          <ColorComponent style="position: absolute; z-index: 300;left: 13%; top: 100px;">
+          </ColorComponent>
+        </v-card-text>
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn
+              class="button"
+              text
+              @click="dialog = false"
+          >
+            Speichern
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
 
     <!-- Hinzufügen Button -->
     <div v-if="user" class="hidden-sm-and-up">
@@ -16,14 +46,16 @@
 
 
 import ColorComponent from "@/components/ColorComponent";
-import { Icon } from '@iconify/vue';
+import {Icon} from '@iconify/vue';
 import {mapGetters} from "vuex";
+
 export default {
 
   data() {
     return {
       farbe: 'blue',
       name: 'Pocket Manager',
+      dialog: false,
     }
   },
   methods: {
@@ -44,7 +76,7 @@ export default {
 }
 </script>
 <style scoped>
-.farbIcon{
+.farbIcon {
   cursor: pointer;
   font-size: 40px;
   position: fixed;
@@ -52,5 +84,24 @@ export default {
   left: 88%;
   top: 80px;
   color: #c20a0a;
+}
+
+.closeIcon {
+  cursor: pointer;
+  font-size: 40px;
+  position: fixed;
+  z-index: 330;
+  left: 88%;
+  top: 80px;
+  color: black;
+}
+
+.dialogCard {
+  height: 400px;
+  width: 400px;
+}
+
+.button {
+  color: v-bind($store.state.color);
 }
 </style>
