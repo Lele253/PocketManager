4cefdcb32c421c3c55cadf473f4672d673c162b6
Stefan <stefan.franke@itc-studenten.de>
2023-01-23 10:02:52 +0100

    regist funzt

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,70 @@
 
     <!-- Hinzufügen Button -->
     <div v-if="user" class="hidden-sm-and-up">
-
+      <v-row justify="center">
+        <v-dialog
+            v-model="dialog"
+            persistent
+        >
+          <template v-slot:activator="{ props }">
+            <v-btn
+                style="top: 600px"
+                position="relative"
+                color="primary"
+                v-bind="props"
+            >
+              Open Dialog
+            </v-btn>
+          </template>
+          <v-card>
+            <v-card-title class="text-center text-h5">
+               Neue Ausgabe
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-row>
+                  <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                  >
+                    <v-text-field
+                        label="was hast du gekauft?"
+                        required
+                    ></v-text-field>
+                  </v-col>
+                  <v-col
+                      cols="12"
+                      sm="6"
+                      md="4"
+                  >
+                    <v-text-field
+                        label="Preis"
+                    ></v-text-field>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                  color="blue-darken-1"
+                  variant="text"
+                  @click="dialog = false"
+              >
+                Close
+              </v-btn>
+              <v-btn
+                  color="blue-darken-1"
+                  variant="text"
+                  @click="dialog = false"
+              >
+                Save
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-row>
     </div>
   </div>
 
@@ -24,6 +87,7 @@ export default {
     return {
       farbe: 'blue',
       name: 'Pocket Manager',
+      dialog: false,
     }
   },
   methods: {
